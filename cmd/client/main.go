a78113dfb9d1578e5a9bd4784f6001cc189f720e
José Garção <garcao.random@gmail.com>
2026-05-01 19:56:15 +0100

    complete lesson 4 of ch4

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -38,6 +38,22 @@ func main() {
 		log.Fatalf("could not subscribe to pause queue: %v\n", err)
 	}
 
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
+	if err := pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.ArmyMovesPrefix+"."+gameState.GetUsername(),
+		routing.ArmyMovesPrefix+".*",
+		pubsub.SimpleQueueTransient,
+		handlerMove(gameState),
+	); err != nil {
+		log.Fatalf("could not subscribe to move queue: %v\n", err)
+	}
+
 	for {
 		words := gamelogic.GetInput()
 		if len(words) == 0 {
@@ -50,10 +66,23 @@ func main() {
 				log.Printf("could not spawn new unit: %v", err)
 			}
 		case "move":
-			_, err := gameState.CommandMove(words)
+			mv, err := gameState.CommandMove(words)
 			if err != nil {
 				log.Printf("could not move unit: %v", err)
+				break
 			}
+
+			if err := pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilTopic,
+				routing.ArmyMovesPrefix+"."+gameState.GetUsername(),
+				mv,
+			); err != nil {
+				log.Printf("could not publish move: %v", err)
+				break
+			}
+
+			log.Println("move message successfuly published!")
 		case "status":
 			gameState.CommandStatus()
 		case "help":
@@ -73,5 +102,12 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(mv gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(mv)
 	}
 }
